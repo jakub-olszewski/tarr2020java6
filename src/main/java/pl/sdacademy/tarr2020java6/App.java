954e852a02509ebed02301af15e426c960ce4dfa
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-02-09 13:53:48 +0100

    Zadanie: Tworzenie metody

--- src/main/java/pl/sdacademy/tarr2020java6/App.java
+++ src/main/java/pl/sdacademy/tarr2020java6/App.java
@@ -2,6 +2,9 @@ package pl.sdacademy.tarr2020java6;
 //generate test
 //alt + enter
 
+
+import static java.lang.Math.PI;
+
 /**
  * App
  *
@@ -18,6 +21,51 @@ public class App {
     public static void main(String[] args) {
         //sout + tab
         System.out.println("Hello World!");
+
+        // dodanie zmiennej Math.PI
+        // typ nazwa = wartosc ;
+        double liczba = 3.0;// zmienna ma wartość 3.0
+        liczba = PI;// zmienna ma wartość 3.141592...
+        liczba = Math.PI;
+        liczba = java.lang.Math.PI;
+
+        //double liczba = 3.6;
+        int number;// deklaracja
+        number = 2020;// inicjalizacja
+
+        int number2 = 6;// deklaracja i inicjalizacja w jednej lini
+
+        System.out.println(number);
+
+        wypiszLiczbe(number2);
+
+        // String - napis;ciąg znaków
+        String odpowiedz = coNaObiad();// odpowiedź metody
+        // jest przypisana do zmiennej odpowiedź
+
+        System.out.println(odpowiedz);
+        System.out.println(coNaObiad());// tutaj nie tworzymy zmiennej
+        System.out.println(coNaObiad("jutro"));// tutaj nie tworzymy zmiennej
+        System.out.println(coNaObiad("pojutrze"));// tutaj nie tworzymy zmiennej
+
+        // Zadanie: Stwórz metodę liczącą sume 3 liczb całkowitych (int)
+
+    }
+
+    // metoda zwraca odpowiedz co na obiad
+    private static String coNaObiad(String kiedy) {
+        return kiedy+" będzie "+"gulasz";
+    }
+
+    // metoda zwraca odpowiedz co na obiad
+    private static String coNaObiad() {
+        return "Pomidorowa z rosołu ze wczoraj";
+    }
+
+
+    // metoda void nic nie zwraca
+    private static void wypiszLiczbe(int liczba) {
+        System.out.println("Twoja liczba to :"+liczba);
     }
 
 }
