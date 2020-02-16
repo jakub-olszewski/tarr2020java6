059d033b9ace1d356543d9a672a32e062e0de978
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-02-16 14:58:19 +0100

    Zadanie: Domowe operatory i dodatkowe ulamki

---
+++ src/main/java/pl/sdacademy/tarr2020java6/zadania/domowe/ZadanieOperatoryMain.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.tarr2020java6.zadania.domowe;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : tarr2020java6
+ * @since : 16.02.2020
+ **/
+public class ZadanieOperatoryMain {
+
+    public static void main(String[] args) {
+
+    }
+}
