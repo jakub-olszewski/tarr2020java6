154e683070f81a13ede93559f6267cc29be33835
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-02-29 14:59:04 +0100

    Zadanie: Pętla for na tablicy

---
+++ src/main/java/pl/sdacademy/tarr2020java6/petle/PetlaForMain.java
@@ -0,0 +1,24 @@
+package pl.sdacademy.tarr2020java6.petle;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : tarr2020java6
+ * @since : 29.02.2020
+ **/
+public class PetlaForMain {
+
+    public static void main(String[] args) {
+
+        int tablicaLiczb[] = new int[3];
+        tablicaLiczb[0] = 20;
+        tablicaLiczb[1] = 19;
+        tablicaLiczb[2] = 18;
+
+        String tablicaMiast[] = {"Toruń","Poznań","Berlin"};
+
+        /**
+         * Zadanie: Wypisz w pętli for elementy tablic
+         */
+
+    }
+}
