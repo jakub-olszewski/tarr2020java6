a344c5c7a68a087c0f98595f9e0d0c87dcc8607d
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-03-01 10:47:09 +0100

    Zadanie: Pętle + rozwiązanie

--- src/main/java/pl/sdacademy/tarr2020java6/petle/ZadanieMain.java
+++ src/main/java/pl/sdacademy/tarr2020java6/petle/ZadanieMain.java
@@ -16,5 +16,17 @@ public class ZadanieMain {
         // przykład :
         //
         //          imie.contains("a")
+        String tablicaImion[] = {"Alf","Jess","Steve","Adam","Anna","Julia","Kinga"};
+
+        for(String imie : tablicaImion){
+            if(imie.contains("a") || imie.contains("A")){
+                System.out.println(imie);
+            }
+            // poniżej alternatywne rozwiązanie
+//            if(imie.toLowerCase().contains("a")){
+//                System.out.println(imie);
+//            }
+        }
+
     }
 }
