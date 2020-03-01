45d4758be72fd89da54ae7d600661aa33d905e3a
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-03-01 14:23:14 +0100

    Rozwiązanie: Polimorfizm, dziedziczenie - utworzenie abstract Figura

--- src/main/java/pl/sdacademy/tarr2020java6/abstrakcja/Figura.java
+++ src/main/java/pl/sdacademy/tarr2020java6/abstrakcja/Figura.java
@@ -8,9 +8,28 @@ import pl.sdacademy.tarr2020java6.abstrakcja.interfejsy.Obliczenia;
  * @project : tarr2020java6
  * @since : 01.03.2020
  **/
-public abstract class Figura{
+public abstract class Figura implements Informacje, Obliczenia{
 
     protected String nazwa;
 
 
+    @Override
+    public void wypiszInformacje() {
+        wypiszNazwe();
+    }
+
+    @Override
+    public void wypiszNazwe() {
+        System.out.println(nazwa);
+    }
+
+    @Override
+    public float obliczPole() {
+        return 0;
+    }
+
+    @Override
+    public float obliczObwod() {
+        return 0;
+    }
 }
