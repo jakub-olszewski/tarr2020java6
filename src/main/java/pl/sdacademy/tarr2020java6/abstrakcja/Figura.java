b5951a2b7c34fa8863c8942985a2966a54c36c26
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-03-01 13:40:40 +0100

    Rozwiązanie: Polimorfizm, dziedziczenie

---
+++ src/main/java/pl/sdacademy/tarr2020java6/abstrakcja/Figura.java
@@ -0,0 +1,16 @@
+package pl.sdacademy.tarr2020java6.abstrakcja;
+
+import pl.sdacademy.tarr2020java6.abstrakcja.interfejsy.Informacje;
+import pl.sdacademy.tarr2020java6.abstrakcja.interfejsy.Obliczenia;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : tarr2020java6
+ * @since : 01.03.2020
+ **/
+public abstract class Figura{
+
+    protected String nazwa;
+
+
+}
---
+++ src/main/java/pl/sdacademy/tarr2020java6/abstrakcja/figury/Kwadrat.java
@@ -0,0 +1,26 @@
+package pl.sdacademy.tarr2020java6.abstrakcja.figury;
+
+import pl.sdacademy.tarr2020java6.abstrakcja.Figura;
+import pl.sdacademy.tarr2020java6.abstrakcja.interfejsy.Informacje;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : tarr2020java6
+ * @since : 01.03.2020
+ **/
+public class Kwadrat extends Figura implements Informacje {
+
+    public Kwadrat(){
+        nazwa = "Kwadrat";
+    }
+
+    @Override
+    public void wypiszInformacje() {
+
+    }
+
+    @Override
+    public void wypiszNazwe() {
+
+    }
+}
