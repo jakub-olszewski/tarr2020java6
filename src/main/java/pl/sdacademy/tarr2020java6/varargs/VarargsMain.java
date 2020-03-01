272e7acbe4e9b4e4171835ccbccd40fcd04cfab7
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-03-01 11:15:32 +0100

    Teoria: Varargs

--- src/main/java/pl/sdacademy/tarr2020java6/varargs/VarargsMain.java
+++ src/main/java/pl/sdacademy/tarr2020java6/varargs/VarargsMain.java
@@ -18,9 +18,38 @@ public class VarargsMain {
             int a = 5, b = 3,c = 7;
             int wynik = suma(a, b,c);
         }
+        {
+            int a = 5, b = 3,c = 7,d = 2;
+            int wynik = suma(a, b,c, d );
+        }
+        {
+            int wynik = suma(12,4,456,45,23,6,3,4,234,7,65,34,34,34,53);
+        }
+        {
+            int wynik = suma(44,45,46);
+            System.out.println("wynik="+wynik);// = 135
+        }
     }
 
-    private static int suma(int a, int b) {
-        return a + b;
+    private static int suma(int... liczby){// tablica liczby
+        // implementacja
+        // sposób działania
+        int suma = 0;
+        for (int liczba : liczby){
+            suma+=liczba;// zwiększamy sume o liczbe
+        }
+        return suma;// wynik metody
     }
+
+    /**
+     * nie potrzebujemy poniższych metod bo jest varargs
+     * dynamiczna ilosc argumentów
+     */
+//    private static int suma(int a, int b, int c) {
+//        return a + b + c;
+//    }
+//
+//    private static int suma(int a, int b) {
+//        return a + b;
+//    }
 }
