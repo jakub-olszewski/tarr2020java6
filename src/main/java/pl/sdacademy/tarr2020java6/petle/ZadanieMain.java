1868a615579dc98e47130051afadfbc797d2cb6b
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-03-01 09:54:54 +0100

    Zadanie: Pętle

---
+++ src/main/java/pl/sdacademy/tarr2020java6/petle/ZadanieMain.java
@@ -0,0 +1,20 @@
+package pl.sdacademy.tarr2020java6.petle;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : tarr2020java6
+ * @since : 01.03.2020
+ **/
+public class ZadanieMain {
+
+    public static void main(String[] args) {
+        // Zadanie
+        //==============
+        // stworzenie tablicy imion
+        // w pętli wypisz imion zawierający literę "a"
+        // skorzystaj z metody contains
+        // przykład :
+        //
+        //          imie.contains("a")
+    }
+}
