397658a2c2bf7bdd41289f97c83658f283777cd2
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-03-01 13:05:05 +0100

    Teoria: Polimorfizm, dziedziczenie

---
+++ src/main/java/pl/sdacademy/tarr2020java6/abstrakcja/AbstrakcjaMain.java
@@ -0,0 +1,24 @@
+package pl.sdacademy.tarr2020java6.abstrakcja;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : tarr2020java6
+ * @since : 01.03.2020
+ **/
+public class AbstrakcjaMain {
+
+    /**
+     * Zadanie
+     *
+     */
+        // Trojkat
+        // Trojkat równoboczny
+        // prostokąt
+        // koło
+        // wszystkie figury tworzymy w pakiecie figury
+
+        /*
+        Zadanie: Stwórz listę figur ,wypisz informacje
+        zsumuj pola;
+         */
+}
