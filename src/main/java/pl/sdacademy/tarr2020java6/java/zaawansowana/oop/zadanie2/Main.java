6d9726c55d87d17f8f7172fa68cda27f9ebc6cd0
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-05-16 14:29:54 +0200

    Zadanie2: Treść zadania

---
+++ src/main/java/pl/sdacademy/tarr2020java6/java/zaawansowana/oop/zadanie2/Main.java
@@ -0,0 +1,39 @@
+package pl.sdacademy.tarr2020java6.java.zaawansowana.oop.zadanie2;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : tarr2019java4-tdd-project
+ * @since : 16.05.2020
+ **/
+public class Main {
+
+    /**
+     * Uwaga. Zadanie dodatkowe dla tych co zrobili poprzednie
+     *
+     * Zadanie2
+     *
+     * Program słownik. Dane wejsciowe w postaci Map (Collection) klucz/wartość.
+     * Słownik ma możliwość wyszukiwania słowa.
+     * Wyszukiwanie poprzez przeglądanie w pętli mapy słów
+     *
+     * Klasa WordData ma być klasą wewnętrzną klasy Dictionary
+     * w klasie Main przykład użycia
+     *
+     * Klasy:
+     * Dictionary - słownik, zawiera WordData
+     * WordData - dane do słownika extends Map
+     * Main - przykład użycia
+     *
+     * Dodatkowo:
+     * 1)
+     * import słów z pliku csv
+     * Przykład:
+     * ---
+     * Mammal,Ssak;
+     * Plant,Roślina;
+     * ---
+     * 1a)
+     * 2)
+     * pobieranie z klawiatury słowa (Scanner)
+     */
+}
