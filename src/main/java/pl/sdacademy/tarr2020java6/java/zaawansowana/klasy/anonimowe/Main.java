16d53ce9e8485e61e3374930f3f8b5952af6f488
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-05-23 10:48:01 +0200

    Zadanie: Klasa anonimowa

---
+++ src/main/java/pl/sdacademy/tarr2020java6/java/zaawansowana/klasy/anonimowe/Main.java
@@ -0,0 +1,59 @@
+package pl.sdacademy.tarr2020java6.java.zaawansowana.klasy.anonimowe;
+
+import pl.sdacademy.tarr2020java6.java.podstawy.abstrakcja.Figura;
+import pl.sdacademy.tarr2020java6.java.podstawy.abstrakcja.figury.Kolo;
+import pl.sdacademy.tarr2020java6.java.podstawy.abstrakcja.figury.Kwadrat;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : tarr2019java4-tdd-project
+ * @since : 23.05.2020
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+
+        Kwadrat kwadrat = new Kwadrat(5);
+        float poleKwadratu = kwadrat.obliczPole();
+        System.out.println(poleKwadratu);
+
+        // Kolo extends Figura
+        Figura kolo = new Kolo(5);
+        float poleKolo = kolo.obliczPole();
+
+        // klasa anonimowa
+        /**
+         * Kwadrat i Koło to klasy które istnieją
+         * w plikach w opowiednich pakietach
+         * klasa anonimowa istnieje tam gdzie ją tworzmy
+         */
+        //Figura prostokat = new Figura();// klasa abstakcjyjna nie można tworzyć
+        // w taki sposób
+        Figura trapez = new Figura() {
+
+            int a = 5;
+            int b = 6;
+            int h = 3;
+            int r = 2;
+
+            @Override
+            public float obliczPole() {
+                return ((a+b)*h)/2;
+            }
+
+            @Override
+            public float obliczObwod() {
+                return a+b+2*r;
+            }
+        };
+        float poleTrapez = trapez.obliczPole();
+        System.out.println(poleTrapez);
+
+
+        /**
+         * Zadanie. Stwórz anonimową klasę zwierzaka,
+         * którego nie masz w klasie (pakiecie)
+         */
+
+    }
+}
