4a317948b67afab25be82c46431e677b9966f3d0
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-05-23 11:34:23 +0200

    Zadanie: Kolekcje

---
+++ src/main/java/pl/sdacademy/tarr2020java6/java/zaawansowana/kolekcje/Main.java
@@ -0,0 +1,64 @@
+package pl.sdacademy.tarr2020java6.java.zaawansowana.kolekcje;
+
+import pl.sdacademy.tarr2020java6.java.podstawy.abstrakcja.Figura;
+import pl.sdacademy.tarr2020java6.java.podstawy.abstrakcja.figury.Kolo;
+import pl.sdacademy.tarr2020java6.java.podstawy.abstrakcja.figury.Kwadrat;
+import pl.sdacademy.tarr2020java6.java.zaawansowana.oop.zadanie1.animals.Animal;
+import pl.sdacademy.tarr2020java6.java.zaawansowana.oop.zadanie1.animals.mammals.Elephant;
+import pl.sdacademy.tarr2020java6.java.zaawansowana.oop.zadanie1.animals.mammals.Lion;
+
+import java.util.*;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : tarr2019java4-tdd-project
+ * @since : 23.05.2020
+ **/
+public class Main {
+
+    /**
+     * Kolekcje
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        /**
+         * Zadanie.
+         *
+         * 1.Podaj przyklady użyć poniższych kolekcji
+         * Zaaragumentuj wybrane użycie
+         *
+         * 2. Co się stanie gdy usuniemy z kolejki ?
+         *
+         * 3.Czym się różni lista od zbioru ?
+         *
+         */
+
+        // lista
+        List list;
+        List<Animal> zoo = new ArrayList<>();
+        zoo.add(new Lion("Simba"));
+        zoo.add(new Elephant("Dumbo"));
+
+        // zbiór
+        Set zbior;
+        Set<Figura> zbiorFigur = new HashSet<>();
+        zbiorFigur.add(new Kwadrat(4));
+        zbiorFigur.add(new Kolo(4));
+
+        // Map
+        Map mapa;
+        Map<Integer,String> mapaLiczb = new HashMap<>();
+        mapaLiczb.put(1,"pierwszy");
+        mapaLiczb.put(2,"drugi");
+
+        // kolejka w kolejnosci dodawania
+        Queue kolejka;
+        Queue<String> kolejkaWBiedronce = new ArrayDeque<>();
+        kolejkaWBiedronce.add("Janusz");
+        kolejkaWBiedronce.add("Grażyna");
+
+
+    }
+}
