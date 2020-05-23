03b2dc668540c29fad8da94fbc66685a61ace947
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-05-23 12:40:05 +0200

    Narzędzia: Wypisz

---
+++ src/main/java/pl/sdacademy/tarr2020java6/java/zaawansowana/narzedzia/Wypisz.java
@@ -0,0 +1,15 @@
+package pl.sdacademy.tarr2020java6.java.zaawansowana.narzedzia;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : tarr2019java4-tdd-project
+ * @since : 23.05.2020
+ **/
+public class Wypisz {
+
+    public static void tytul(String tytul){
+        System.out.println("============================");
+        System.out.println("\t "+tytul);
+        System.out.println("----------------------------");
+    }
+}
