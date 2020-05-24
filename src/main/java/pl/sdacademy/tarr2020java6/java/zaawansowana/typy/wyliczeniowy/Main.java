d51fc3162804b4ec9c1480ce8abd6df80a42a0b0
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-05-24 12:46:33 +0200

    Zadanie: Typ wyliczeniowy

---
+++ src/main/java/pl/sdacademy/tarr2020java6/java/zaawansowana/typy/wyliczeniowy/Main.java
@@ -0,0 +1,38 @@
+package pl.sdacademy.tarr2020java6.java.zaawansowana.typy.wyliczeniowy;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : tarr2019java4-tdd-project
+ * @since : 24.05.2020
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        Plec plecMezczyzny = Plec.MEZCZYZNA;
+        Plec plecKobiety = Plec.KOBIETA;
+
+        // poniżej możemy poplenić błąd
+        String mezczyzna = "meszczyzna";
+        String kobieta = "kobieta";
+        kobieta = "kobitka";
+
+        Plec wprowadzonaPlec = Plec.KOBIETA;
+        switch (wprowadzonaPlec){
+            case MEZCZYZNA:
+                System.out.println("M");
+                break;
+            case KOBIETA:
+                System.out.println("K");
+                break;
+        }
+
+        /**
+         * Zadanie.
+         * Stworzeniu enum FormatPapieru
+         * A1, A2, A3, A4
+         * posiada metody:
+         * - pobierzWyskosc
+         * - pobierzSzerokosc
+         */
+    }
+}
