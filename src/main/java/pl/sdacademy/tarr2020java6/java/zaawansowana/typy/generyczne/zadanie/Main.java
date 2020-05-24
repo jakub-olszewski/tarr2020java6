866a86730781d95f0d26b87815e683327ed65fbc
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-05-24 09:43:02 +0200

    Zadanie: Interfejs generyczny

---
+++ src/main/java/pl/sdacademy/tarr2020java6/java/zaawansowana/typy/generyczne/zadanie/Main.java
@@ -0,0 +1,27 @@
+package pl.sdacademy.tarr2020java6.java.zaawansowana.typy.generyczne.zadanie;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : tarr2019java4-tdd-project
+ * @since : 24.05.2020
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        /**
+         * Zadanie.
+         * 
+         * Main:
+         * Ogrodnik, który sadzi rośliny w doniczki i podlewa
+         *
+         * Klasy:
+         * - Ogrodnik - klasa
+         * - PraceOgrodnika - interfejs <> rozszerzający Plant
+         *      * sadzenie
+         *      * podlewanie
+         *      * zbieranieOwocow
+         * - Doniczka - klasa generyczna extends Plant
+         *
+         */
+    }
+}
