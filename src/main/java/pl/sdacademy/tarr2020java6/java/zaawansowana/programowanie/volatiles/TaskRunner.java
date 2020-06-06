60c8f90c4203c3df92ec70aa30b8e990e739c2da
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-06-06 15:09:50 +0200

    Teoria: Volatile

---
+++ src/main/java/pl/sdacademy/tarr2020java6/java/zaawansowana/programowanie/volatiles/TaskRunner.java
@@ -0,0 +1,31 @@
+package pl.sdacademy.tarr2020java6.java.zaawansowana.programowanie.volatiles;
+
+import pl.sdacademy.tarr2020java6.java.zaawansowana.programowanie.Timer;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : tarr2019java4-tdd-project
+ * @since : 02.06.2020
+ **/
+public class TaskRunner {
+
+    public static int number;
+    public static boolean ready;
+
+    public static class Reader extends Thread {
+
+        @Override
+        public void run() {
+            Timer timer = new Timer("thread");
+            timer.start();
+            while (!ready) {
+                Thread.yield();
+            }
+
+            System.out.println(number);
+            timer.stop();
+            timer.print();
+        }
+    }
+
+}
